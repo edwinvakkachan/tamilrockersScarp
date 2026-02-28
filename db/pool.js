e65f5ea2da20d81052db82b73821e79c1d560826
edwin vakkachan <edwinvakkachan@gmail.com>
2026-02-28 23:31:55 +0530

    deleted

--- db/pool.js
+++ db/pool.js
@@ -1,25 +1,12 @@
 import pkg from "pg";
 const { Pool } = pkg;
-import { retry } from "../homeassistant/retryWrapper";
-import { triggerHomeAssistantWebhookWhenErrorOccurs } from "../homeassistant/homeAssistantWebhook";
 
-try {
-  const pool = new Pool({
+ const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false },
     max: 10,
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 10000,
   });
-} catch (error) {
-  console.error("supabase db  insert failed:", err.message);
-       await retry(
-    triggerHomeAssistantWebhookWhenErrorOccurs,
-    { status: "error" },
-    "homeassistant-error",
-    5
-  );
-  process.exit(1)
-}
 
 export default pool;
--- queue/publishMessage.js
+++ queue/publishMessage.js
@@ -18,12 +18,25 @@ export async function publishMessage({
     time: new Date().toISOString()
   };
 
-  await pool.query(
-    `
-    INSERT INTO app_message_queue
-    (source_app, event_type, payload, target, scheduled_at)
-    VALUES ($1,$2,$3,$4, COALESCE($5, CURRENT_TIMESTAMP))
-    `,
-    [sourceApp, eventType, payload, target, scheduledAt]
+ try {
+   await pool.query(
+     `
+     INSERT INTO app_message_queue
+     (source_app, event_type, payload, target, scheduled_at)
+     VALUES ($1,$2,$3,$4, COALESCE($5, CURRENT_TIMESTAMP))
+     `,
+     [sourceApp, eventType, payload, target, scheduledAt]
+   );
+ } catch (error) {
+  console.error("Queue insert failed:", err.message);
+        await retry(
+    triggerHomeAssistantWebhookWhenErrorOccurs,
+    { status: "error" },
+    "homeassistant-error",
+    5
   );
+
+  process.exit(1);
+
+ }
 }
