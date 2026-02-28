294d3ac000f92837babe86c5fed934f3efb16490
edwin vakkachan <edwinvakkachan@gmail.com>
2026-02-28 23:25:36 +0530

    supabase support

--- db/pool.js
+++ db/pool.js
@@ -1,12 +1,24 @@
 import pkg from "pg";
 const { Pool } = pkg;
+import { retry } from "../homeassistant/retryWrapper";
+import { triggerHomeAssistantWebhookWhenErrorOccurs } from "../homeassistant/homeAssistantWebhook";
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
-  max: 10,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 10000,
-});
+try {
+  const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+    max: 10,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 10000,
+  });
+} catch (error) {
+  console.error("supabase db  insert failed:", err.message);
+       await retry(
+    triggerHomeAssistantWebhookWhenErrorOccurs,
+    { status: "error" },
+    "homeassistant-error",
+    5
+  );
+}
 
 export default pool;
