ee6cad96e5841a99b732c099076e55ab61c28d6a
edwin vakkachan <edwinvakkachan@gmail.com>
2026-03-01 01:23:57 +0530

    text

--- extractHomePage.js
+++ extractHomePage.js
@@ -40,14 +40,17 @@ $("a[href]").each((_, element) => {
     : rawHref;
 
   const lowerHref = href.toLowerCase();
-  const text = $(element).text().trim().toLowerCase();
+  
+  // const text = $(element).text().trim().toLowerCase();
+
+  const text = $(element).parent().text().trim().toLowerCase();
 
   // Must be topic page
   if (!lowerHref.includes("/forums/topic/")) return;
 
   // Skip PreDVD (URL or title)
   if (lowerHref.includes("predvd") || text.includes("predvd")) return;
-  
+
 // checking enlgish or not 
   if (lowerHref.includes("english") || text.includes("english")) return;
 
