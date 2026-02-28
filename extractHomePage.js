0741054ea8745bce31fcd7fd482b6d88ce9f1f9e
edwin vakkachan <edwinvakkachan@gmail.com>
2026-03-01 02:19:33 +0530

    errors fixed

--- extractHomePage.js
+++ extractHomePage.js
@@ -51,19 +51,21 @@ $("a[href]").each((_, element) => {
   // Skip PreDVD (URL or title)
   if (lowerHref.includes("predvd") || text.includes("predvd")) return;
 
-// checking enlgish or not 
+  // Combine text + URL for single language check
+  const searchableContent = `${text} ${lowerHref}`;
 
+  
+  // checking enlgish or not 
+  
   if (lowerHref.includes("english") || text.includes("english")) return;
-
-
+  
+  
   if (/\beng\b/i.test(searchableContent)) return;
 
   // ignore chinese 
 
   if (/\bchi\b/i.test(searchableContent)) return;
 
-  // Combine text + URL for single language check
-  const searchableContent = `${text} ${lowerHref}`;
 
   // Must contain allowed language
   if (!languageRegex.test(searchableContent)) return;
